6ae92c4487a94be960c5ae33afe9be36114727c5
alex2019d18 <droidbrew@gmail.com>
2019-11-19 21:06:46 +0300

    Add mysql adapter + remove camelcase

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,47 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_191_103_072_532) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2019_11_03_072532) do
 
-  create_table 'divisions', force: :cascade do |t|
-    t.string 'name', null: false
-    t.bigint 'tournament_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['tournament_id'], name: 'index_divisions_on_tournament_id'
+  create_table "divisions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "tournament_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tournament_id"], name: "index_divisions_on_tournament_id"
   end
 
-  create_table 'matches', force: :cascade do |t|
-    t.string 'result', null: false
-    t.string 'winner', null: false
-    t.string 'stage', null: false
-    t.bigint 'home_team_id', null: false
-    t.bigint 'guest_team_id', null: false
-    t.bigint 'tournament_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['guest_team_id'], name: 'index_matches_on_guest_team_id'
-    t.index ['home_team_id'], name: 'index_matches_on_home_team_id'
-    t.index ['tournament_id'], name: 'index_matches_on_tournament_id'
+  create_table "matches", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "result", null: false
+    t.string "winner", null: false
+    t.string "stage", null: false
+    t.bigint "home_team_id", null: false
+    t.bigint "guest_team_id", null: false
+    t.bigint "tournament_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["guest_team_id"], name: "index_matches_on_guest_team_id"
+    t.index ["home_team_id"], name: "index_matches_on_home_team_id"
+    t.index ["tournament_id"], name: "index_matches_on_tournament_id"
   end
 
-  create_table 'teams', force: :cascade do |t|
-    t.string 'name', null: false
-    t.integer 'division_points', default: 0
-    t.bigint 'tournament_id', null: false
-    t.bigint 'division_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['division_id'], name: 'index_teams_on_division_id'
-    t.index ['tournament_id'], name: 'index_teams_on_tournament_id'
+  create_table "teams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "division_points", default: 0
+    t.bigint "tournament_id", null: false
+    t.bigint "division_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["division_id"], name: "index_teams_on_division_id"
+    t.index ["tournament_id"], name: "index_teams_on_tournament_id"
   end
 
-  create_table 'tournaments', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'winner'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "tournaments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "winner"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
 end
